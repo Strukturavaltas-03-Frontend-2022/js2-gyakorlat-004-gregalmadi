9d0267502c417039a76dba6da78f5495be4175d6
Kawi <kawiwinter@gmail.com>
2022-08-07 02:58:02 +0200

    js2_004

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,14 @@
+const countOfWorkingDays = () => {
+  const date1 = new Date(`August 31, 2022`);
+  const date2 = new Date(`January 01, 2022`);
 
+  const weeks = (date1.getTime() - date2.getTime()) / 1000 / 60 / 60 / 24 / 7;
+  const fullWeeks = Math.trunc(weeks);
+  const remainder = weeks - Math.trunc(weeks);
 
+  const weekdays = fullWeeks * 5 + Math.trunc(remainder * 7);
 
+  return weekdays;
+};
 
-
-
-// export default countOfWorkingDays;
+export default countOfWorkingDays;
